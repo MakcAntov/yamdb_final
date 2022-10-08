abb95b0193cd096aea053452be0ed75b48ad4324
Makc Antov <makcant@yandex.ru>
2022-10-08 17:29:05 +0400

    FIX v3.1

--- api_yamdb/api/management/commands/load_from_csv.py
+++ api_yamdb/api/management/commands/load_from_csv.py
@@ -3,8 +3,7 @@ import os
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
-
-from reviews.models import Category, Genre, Title, Comment, Review
+from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import User
 
 
